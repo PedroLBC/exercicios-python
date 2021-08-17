8cb25c9fb61bbef3cac443385e3131f80bac730f
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-17 10:59:28 -0300

    Update Desafio 15.py

--- Desafio 15.py
+++ Desafio 15.py
@@ -1,4 +1,6 @@
 print('\033[1;36;40mDESAFIO 15 - AULA 07\033[m\n')
+## Exercício Python 15: Escreva um programa que pergunte a quantidade de Km percorridos por um carro alugado e a quantidade de dias pelos quais ele foi alugado. 
+# Calcule o preço a pagar, sabendo que o carro custa R$60 por dia e R$0,15 por Km rodado.
 dia = float(input('Quantos dias o carro ficou alugado? '))
 km = float(input('Quantos km foram percorridos com o carro? '))
 v1 = dia * 60
