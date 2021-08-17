029d5d4bf018ae042a86aea2205cd0ba3ccee1e0
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-17 10:58:55 -0300

    Update Desafio 14.py

--- Desafio 14.py
+++ Desafio 14.py
@@ -1,4 +1,5 @@
 print('\033[1;36;40mDESAFIO 14 - AULA 07\033[m\n')
+## Exercício Python 14: Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit.
 c = float(input('Coloque uma temperatura em Celsius(ºC): '))
 print('\033[31mConvertendo para Kelvin dará {}K\033[m\n\033[32mConvervendo para Fahrenheit dará {}ºF\033[m'.format((c + 273.15), ((c * 9/5) + 32)))
 input('\n')
