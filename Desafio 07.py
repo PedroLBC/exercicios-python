65c27f13bc3aa49ca102a712a4437b5f4cad09f2
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-17 10:53:53 -0300

    Update Desafio 07.py

--- Desafio 07.py
+++ Desafio 07.py
@@ -1,4 +1,5 @@
 print('\033[1;36;40mDESAFIO 07 - AULA 07\033[m\n')
+## Exercício Python 7: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
 nome = input('Olá aluno, qual seu nome? ')
 print('\033[32m{}\033[m, farei uma média das suas notas.'.format(nome))
 n1 = float(input('Qual nota vc teve no 1 trimestre? '))
