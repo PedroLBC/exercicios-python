47699cb38129ac7a32024043b0ae81ad4a63e385
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-17 10:56:25 -0300

    Update Desafio 11.py

--- Desafio 11.py
+++ Desafio 11.py
@@ -1,4 +1,6 @@
 print('\033[1;36;40mDESAFIO 11 - AULA 07\033[m\n')
+## Exercício Python 11: Faça um programa que leia a largura e a altura de uma parede em metros, 
+# calcule a sua área e a quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma área de 2 metros quadrados.
 com = float(input('Referente a uma parede, qual o comprimento dela (em metros)? '))
 larg = float(input('E a largura (em metros)? '))
 area = com * larg
