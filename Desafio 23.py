712f2ee664a6b9826893cb3dbd45f7b82fdd4ab1
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-18 11:02:43 -0300

    Update Desafio 23.py

--- Desafio 23.py
+++ Desafio 23.py
@@ -1,4 +1,6 @@
 print('\033[1;36;40mDESAFIO 23 - AULA 09\033[m\n')
+## Exercício Python 23: Faça um programa que leia um número de 0 a 9999 e mostre na tela cada um dos dígitos separados.
+
 inteiro = int(input('Coloque um numero entre 0 a 9999: '))
 if inteiro <= 9:
     print('\033[35mUnidade: {}\nDezena: {}\nCentena: {}\nMilhar: {}\033[m'.format(inteiro, 0, 0, 0))
