514738692d8ad2a069c15e635745e71b744c121d
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-18 11:05:56 -0300

    Update Desafio 26.py

--- Desafio 26.py
+++ Desafio 26.py
@@ -1,6 +1,7 @@
 print('\033[1;36;40mDESAFIO 26 - AULA 09\033[m\n')
 ## Exercício Python 26: Faça um programa que leia uma frase pelo teclado e mostre quantas vezes aparece a letra “A”, 
 # em que posição ela aparece a primeira vez e em que posição ela aparece a última vez.
+
 frase = input('Digite uma frase: ').strip()
 frase1 = frase.upper()
 print('Na frase aparece a letra "a" \033[36m{}\033[m vez(es)'.format(frase1.count('A')))
