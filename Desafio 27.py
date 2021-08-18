5c392641cd84ebc675a02bdd579211d80360129f
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-18 11:06:41 -0300

    Update Desafio 27.py

--- Desafio 27.py
+++ Desafio 27.py
@@ -1,4 +1,6 @@
 print('\033[1;36;40mDESAFIO 27 - AULA 09\033[m\n')
+## Exercício Python 27: Faça um programa que leia o nome completo de uma pessoa, mostrando em seguida o primeiro e o último nome separadamente.
+
 nome = input('Digite seu nome completo: ').upper().strip()
 nome1 = nome.lower().title().split()
 print('Seu primeiro nome é: \033[34m{}\033[m'.format(nome1[0]))
