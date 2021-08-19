699df04ee7cf186df8842b3f11693b6f486976b3
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-19 20:20:03 -0300

    Update Desafio 38.py

--- Mundo 2/Aula 12/Desafio 38.py
+++ Mundo 2/Aula 12/Desafio 38.py
@@ -2,7 +2,7 @@ print('\033[1;36;40mDESAFIO 38 - AULA 12\033[m\n')
 num1 = int(input('Digite um número: '))
 num2 = int(input('Digite outro número: '))
 
-## Exercício Python 38: Escreva um programa que leia dois números inteiros e compare-os. mostrando na tela uma mensagem:
+## Exercício Python 38: Escreva um programa que leia dois números inteiros e compare-os mostrando na tela uma mensagem:
 # – O primeiro valor é maior
 # – O segundo valor é maior
 # – Não existe valor maior, os dois são iguais
