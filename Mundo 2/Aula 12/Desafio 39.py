75031f3c079e0edfeffc10d39a6e1c427f551ba9
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-19 20:20:49 -0300

    Update Desafio 39.py

--- Mundo 2/Aula 12/Desafio 39.py
+++ Mundo 2/Aula 12/Desafio 39.py
@@ -1,6 +1,8 @@
 print('\033[1;36;40mDESAFIO 39 - AULA 12\033[m\n')
 
-## Exercício Python 39: Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+## Exercício Python 39: Faça um programa que leia o ano de nascimento de um jovem e informe, de acordo com a sua idade, 
+# se ele ainda vai se alistar ao serviço militar, se é a hora exata de se alistar ou se já passou do tempo do alistamento. 
+# Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
 
 idade = int(input('Qual sua idade? '))
 
