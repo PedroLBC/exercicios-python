4f3da407be9602d53bcb6c7d9354552c7b68abce
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-24 11:38:06 -0300

    Update Desafio 54.py

--- Mundo 2/Aula 13/Desafio 54.py
+++ Mundo 2/Aula 13/Desafio 54.py
@@ -1,5 +1,7 @@
 print('\033[1;36;40mDESAFIO 54 - AULA 13\033[m')
-## Exercício Python 54: Crie um programa que leia o ano de nascimento de sete pessoas. No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+## Exercício Python 54: Crie um programa que leia o ano de nascimento de sete pessoas. 
+# No final, mostre quantas pessoas ainda não atingiram a maioridade e quantas já são maiores.
+
 a = 0
 b = 0
 for c in range(1, 8):
