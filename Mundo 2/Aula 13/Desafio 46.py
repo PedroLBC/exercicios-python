1e9ecf93e07a83e86e135c14cddab435ce4510f3
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-24 11:32:21 -0300

    Update Desafio 46.py

--- Mundo 2/Aula 13/Desafio 46.py
+++ Mundo 2/Aula 13/Desafio 46.py
@@ -2,6 +2,7 @@ from time import sleep
 print('\033[1;36;40mDESAFIO 46 - AULA 13\033[m')
 ## Exercício Python 46: Faça um programa que mostre na tela uma contagem regressiva para o estouro de fogos de artifício, 
 # indo de 10 até 0, com uma pausa de 1 segundo entre eles.
+
 print('A contagem começa em 10 segundos!')
 for c in range(10, 0, -1):
     n = c
