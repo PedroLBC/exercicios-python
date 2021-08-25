c6e27850336442d126e22dd5e454884d4bd0aac4
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-25 11:00:50 -0300

    Update Desafio 57.py

--- Mundo 2/Aula 14/Desafio 57.py
+++ Mundo 2/Aula 14/Desafio 57.py
@@ -1,4 +1,6 @@
 print('\033[1;36;40mDESAFIO 57 - AULA 13\033[m')
+## Exercício Python 57: Faça um programa que leia o sexo de uma pessoa, mas só aceite os valores ‘M’ ou ‘F’. 
+# Caso esteja errado, peça a digitação novamente até ter um valor correto.
 
 sexo = input('Qual seu gênero? [M / F] ').upper().strip()[0]
 while not sexo in 'MF':
