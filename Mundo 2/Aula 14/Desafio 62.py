059d53382d014159b5f93468b0a02bb2b0aaf18f
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-25 11:07:05 -0300

    Update Desafio 62.py

--- Mundo 2/Aula 14/Desafio 62.py
+++ Mundo 2/Aula 14/Desafio 62.py
@@ -1,6 +1,7 @@
 from desafio import atividade
 atividade(62, 14)
-
+## Exercício Python 62: Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos. 
+# O programa encerrará quando ele disser que quer mostrar 0 termos.
 num = int(input('Qual o primeiro termo? '))
 raz = int(input('Qual a razão? '))
 contagem = int(input('Com quantas vezes você quer que a PA apareça? ')) + 1
