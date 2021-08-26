b25cdedbf2e5444996b3d3442d603a006d5c8e57
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-26 00:03:39 -0300

    Update Desafio 67.py

--- Mundo 2/Aula 15/Desafio 67.py
+++ Mundo 2/Aula 15/Desafio 67.py
@@ -1,6 +1,7 @@
 from desafio import atividade
 atividade(67, 15)
-
+## Exercício Python 67: Faça um programa que mostre a tabuada de vários números, um de cada vez, para cada valor digitado pelo usuário. 
+# O programa será interrompido quando o número solicitado for negativo.
 while True:
     num = int(input('Digite um número: [número negativo para parar] '))
     if num <= 0:
