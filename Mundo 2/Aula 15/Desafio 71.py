d6802a39e71beaf1d3cde1f29d6886ec8024079c
Pedro Lucas <88911645+PedroLBC@users.noreply.github.com>
2021-08-26 00:07:14 -0300

    Update Desafio 71.py

--- Mundo 2/Aula 15/Desafio 71.py
+++ Mundo 2/Aula 15/Desafio 71.py
@@ -1,5 +1,8 @@
 from desafio import atividade
 atividade(71, 15)
+## Exercício Python 071: Crie um programa que simule o funcionamento de um caixa eletrônico. 
+# No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) e o programa vai informar quantas cédulas de cada valor serão entregues. 
+# OBS: considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
 
 num = int(input('Qual o valor que você quer sacar? R$'))
 
